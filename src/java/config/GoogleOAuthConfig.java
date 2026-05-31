5e3b7e2de7d65b971de478bfa7297ec36060a2b0
tinnvt-co <tinnvthe172346@fpt.edu.vn>
2026-05-31 20:44:25 +0700

    update config google auth

--- src/java/config/GoogleOAuthConfig.java
+++ src/java/config/GoogleOAuthConfig.java
@@ -1,13 +1,36 @@
 package config;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 public class GoogleOAuthConfig {
 
-    public static final String CLIENT_ID     = "132031612293-1eu6r0nudgut0j9v9fulh3hdkc937136.apps.googleusercontent.com";
-    public static final String CLIENT_SECRET = "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";
-    public static final String REDIRECT_URI  = "http://localhost:8080/SWP391_HRM_group7/auth/google/callback";
+    public static final String CLIENT_ID;
+    public static final String CLIENT_SECRET;
+    public static final String REDIRECT_URI;
 
     public static final String AUTH_URL      = "https://accounts.google.com/o/oauth2/v2/auth";
     public static final String TOKEN_URL     = "https://oauth2.googleapis.com/token";
     public static final String USERINFO_URL  = "https://www.googleapis.com/oauth2/v3/userinfo";
     public static final String SCOPE         = "openid email profile";
+
+    static {
+        Properties props = new Properties();
+        try (InputStream in = GoogleOAuthConfig.class.getClassLoader()
+                .getResourceAsStream("oauth.properties")) {
+            if (in != null) props.load(in);
+        } catch (IOException ignored) {}
+
+        CLIENT_ID     = resolve(props, "google.client.id",     "GOOGLE_CLIENT_ID");
+        CLIENT_SECRET = resolve(props, "google.client.secret", "GOOGLE_CLIENT_SECRET");
+        REDIRECT_URI  = resolve(props, "google.redirect.uri",  "GOOGLE_REDIRECT_URI");
+    }
+
+    private static String resolve(Properties props, String propKey, String envKey) {
+        String env = System.getenv(envKey);
+        if (env != null && !env.isBlank()) return env;
+        String value = props.getProperty(propKey);
+        return value != null ? value.trim() : "";
+    }
 }
